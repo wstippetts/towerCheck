4777c031a4a5da050183fa3fa026af3a94766f03
Scott Tippetts <wstippetts+github@gmail.com>
2023-03-02 15:13:33 -0700

    clear event details page

--- towerCheck.client/src/pages/EventDetailsPage.vue
+++ towerCheck.client/src/pages/EventDetailsPage.vue
@@ -1,59 +1,18 @@
 <template>
   <div class="EventDetails">
-    <div v-for="ticket in tickets">
-      <img class="rounded" :src="ticket.profile.picture">
-    </div>
+
 
   </div>
 </template>
 
 
 <script>
-import { onMounted, computed, watchEffect } from 'vue';
-import Pop from "../utils/Pop.js"
-import { attendeesService } from "../services/AttendeesService.js";
-import { eventsService } from "../services/EventsService.js";
-import { AppState } from "../AppState.js";
-import { useRoute, useRouter } from "vue-router";
-
 export default {
   setup() {
-    const route = useRoute()
-    const router = useRouter()
-
-    async function getTicketsByEventId() {
-      const eventId = route.params.eventId
-      try {
-        await attendeesService.getTicketsByEventId(eventId)
-      } catch (error) {
-        Pop.error(error)
-      }
-    }
-    async function getEventById() {
-      const eventId = route.params.eventId
-      try {
-        await eventsService.getEventById(eventId)
-      } catch (error) {
-        Pop.error(error)
-      }
-    }
+    return {}
   }
 }
-
-onMounted(() => {
-
-  getTicketsByEventId()
-  getEventById()
-  // getCommentsByEventId()
-
-})
-return {
-  // tickets: computed(()=> )
-}
-
-
 </script>
 
 
 <style lang="scss" scoped></style>
-
